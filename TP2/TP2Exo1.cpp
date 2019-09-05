951411b84c629c9ed3a1dfbc4a9a3e8649c7b8ae
julefb <jules.lefebvre@polymtl.ca>
2019-09-05 12:51:51 -0400

    TP2: exo 1 compile. À tester sur robot.

--- TP2/TP2Exo1.cpp
+++ TP2/TP2Exo1.cpp
@@ -3,36 +3,34 @@
 * Table des états
 */
 
+#define F_CPU 8192000UL
 #include <avr/io.h>
+#include <util/delay_basic.h>
+#include <util/delay.h>
 
 const uint8_t ETEINT = 0;
 const uint8_t ROUGE = 2;
 
-//fonction qui effectue la lecture du bouton poussoir avec anti-rebond
+// Fonction qui effectue la lecture du bouton poussoir avec anti-rebond
 bool lecture_bouton_poussoir(){
-
-    //lecture 1 bouton
-    bool etat_bouton_poussoir_1 = PIND & 0x04;
-
-    //delai (antirebond) d'environ 10 millisecondes 
-    _delay_ms(10);
-    //lecture 2 bouton
-    bool etat_bouton_poussoir_2 = PIND & 0x04;
+    bool etat_bouton_poussoir_1 = PIND & 0x04; // lecture 1
+    _delay_ms(10); // délai (antirebond) d'environ 10 millisecondes 
+    bool etat_bouton_poussoir_2 = PIND & 0x04; // lecture 2
 
     return etat_bouton_poussoir_1 && etat_bouton_poussoir_2;
 }
 
 int main(){
     DDRD = 0;   // PORT D est en mode entrée
-    DDRB = 0xff // PORT B est en mode sortie
+    DDRB = 0xff; // PORT B est en mode sortie
 
     for(;;){
         PORTB = ETEINT; // La DEL est éteinte par défaut
         
         // La boucle for se répête tant que le bouton-pressoir n'a pas été pressé et relâché 5 fois
-        for(int compteurBouton = 0; i < 5;){ 
-            if(lecture_bouton_poussoir){        
-                if(!lecture_bouton_poussoir){ 
+        for(unsigned int compteurBouton = 0; compteurBouton < 5;){ 
+            if(lecture_bouton_poussoir()){        
+                if(!lecture_bouton_poussoir()){ 
                     compteurBouton++;   // Si le bouton est enfoncé et relâché le compteur est incrémenté.
                 }
             }
@@ -40,7 +38,7 @@ int main(){
 
         // La DEL passe au rouge pour 8 192 000 cycles d'horloge (1 sec à 8,192 MHz) 
         PORTB = ROUGE; 
-        for(int compteurDelai = 0; i < 8192000; i++){}
+        for(unsigned long compteurDelai = 0; compteurDelai < 8192000; compteurDelai++){}
     }    
 
     return 0;
