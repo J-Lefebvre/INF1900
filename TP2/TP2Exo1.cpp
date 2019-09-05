e45bb71f1806038ab2ab889e72eeeb10d8778b85
julefb <jules.lefebvre@polymtl.ca>
2019-09-05 13:02:24 -0400

    TP2 exo1 : Ajout de la table des états en commentaire.

--- TP2/TP2Exo1.cpp
+++ TP2/TP2Exo1.cpp
@@ -1,6 +1,22 @@
 /*
-* Nom : Exo 1 TP2
-* Table des états
+* Exercice 1 du TP2
+*
+* TABLE DES ÉTATS
+*
+* ÉTAT PRÉSENT | BOUTON | ÉTAT SUIVANT | DEL
+*     INIT     |   0    |     INIT     |  0
+*     INIT     |   1    |      E1      |  0
+*      E1      |   0    |      E1      |  0
+*      E1      |   1    |      E2      |  0
+*      E2      |   0    |      E2      |  0
+*      E2      |   1    |      E3      |  0
+*      E3      |   0    |      E3      |  0
+*      E3      |   1    |      E4      |  0
+*      E4      |   0    |      E4      |  0
+*      E4      |   1    |     INIT     |  1
+*
+* BOUTON : 1 signifie que le bouton a été pressé et relâché.
+* DEL : 1 signifie que la DEL scintille en rouge pour 1 seconde.
 */
 
 #define F_CPU 8192000UL
